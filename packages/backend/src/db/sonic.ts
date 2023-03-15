b393ab1124a59086de85efbf99375e07fa1b1884
Kaitlyn Allan <kaitlyn.allan@enlabs.cloud>
2023-03-16 06:15:16 +1000

    Set defaults for sonic config

--- packages/backend/src/db/sonic.ts
+++ packages/backend/src/db/sonic.ts
@@ -4,17 +4,17 @@ import config from "@/config/index.js";
 
 const search = config.sonic
 	? new SonicChannel.Search({
-			host: config.sonic.host,
-			port: config.sonic.port,
-			auth: config.sonic.auth,
+			host: config.sonic.host ?? "localhost",
+			port: config.sonic.port ?? 1491,
+			auth: config.sonic.auth ?? "SecretPassword",
 		}).connect({})
 	: null;
 
 const ingest = config.sonic
 	? new SonicChannel.Ingest({
-			host: config.sonic.host,
-			port: config.sonic.port,
-			auth: config.sonic.auth,
+			host: config.sonic.host ?? "localhost",
+			port: config.sonic.port ?? 1491,
+			auth: config.sonic.auth ?? "SecretPassword",
 		}).connect({})
 	: null;
 
