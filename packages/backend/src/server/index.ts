437e3e68576910bec367c917337472b99c46585c
Kaitlyn Allan <kaitlyn.allan@enlabs.cloud>
2023-03-16 06:53:56 +1000

    force sonic to load at start

--- packages/backend/src/server/index.ts
+++ packages/backend/src/server/index.ts
@@ -29,6 +29,8 @@ import proxyServer from "./proxy/index.js";
 import webServer from "./web/index.js";
 import { initializeStreamingServer } from "./api/streaming.js";
 
+import "./db/sonic.js";
+
 export const serverLogger = new Logger("server", "gray", false);
 
 // Init app
