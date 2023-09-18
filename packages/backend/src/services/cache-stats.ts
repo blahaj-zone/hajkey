e67f1a8d4c432b83d0d8f2c562132f2dc8aa6485
Kaity A <supakaity@blahaj.zone>
2023-09-18 14:16:41 +1000

    centralize stats reporting

--- packages/backend/src/services/cache-stats.ts
+++ packages/backend/src/services/cache-stats.ts
@@ -1,37 +1,45 @@
 import Logger from "./logger";
-import { localUserByIdCache, localUserByNativeTokenCache, uriPersonCache, userByIdCache } from "./user-cache";
-import { publicKeyByUserIdCache, publicKeyCache } from "../remote/activitypub/db-resolver.js";
+import {
+	localUserByIdCache,
+	localUserByNativeTokenCache,
+	uriPersonCache,
+	userByIdCache,
+} from "./user-cache";
+import {
+	publicKeyByUserIdCache,
+	publicKeyCache,
+} from "../remote/activitypub/db-resolver.js";
 
 const logger = new Logger("cache-stats");
 
-export const statsScheduler = () => { 
-setInterval(() => {
-	const logs = [];
-	if (userByIdCache.used)
-		logs.push(
-			`userById: ${userByIdCache.hitCount}:${userByIdCache.missCount}, ${userByIdCache.notFoundCount} n/f`,
-		);
-	if (localUserByNativeTokenCache.used)
-		logs.push(
-			`localUserByNativeToken: ${localUserByNativeTokenCache.hitCount}:${localUserByNativeTokenCache.missCount}, ${localUserByNativeTokenCache.notFoundCount} n/f`,
-		);
-	if (localUserByIdCache.used)
-		logs.push(
-			`localUserById: ${localUserByIdCache.hitCount}:${localUserByIdCache.missCount}, ${localUserByIdCache.notFoundCount} n/f`,
-		);
-	if (uriPersonCache.used)
-		logs.push(
-			`uriPerson: ${uriPersonCache.hitCount}:${uriPersonCache.missCount}, ${uriPersonCache.notFoundCount} n/f`,
-		);
-	if (publicKeyCache.used)
-		logs.push(
-			`publicKey: ${publicKeyCache.hitCount}:${publicKeyCache.missCount}, ${publicKeyCache.notFoundCount} n/f`,
-		);
-	if (publicKeyByUserIdCache.used)
-		logs.push(
-			`publicKeyByUserId: ${publicKeyByUserIdCache.hitCount}:${publicKeyByUserIdCache.missCount}, ${publicKeyByUserIdCache.notFoundCount} n/f`,
-		);
+export const statsScheduler = () => {
+	setInterval(() => {
+		const logs = [];
+		if (userByIdCache.used)
+			logs.push(
+				`userById: ${userByIdCache.hitCount}:${userByIdCache.missCount}, ${userByIdCache.notFoundCount} n/f`,
+			);
+		if (localUserByNativeTokenCache.used)
+			logs.push(
+				`localUserByNativeToken: ${localUserByNativeTokenCache.hitCount}:${localUserByNativeTokenCache.missCount}, ${localUserByNativeTokenCache.notFoundCount} n/f`,
+			);
+		if (localUserByIdCache.used)
+			logs.push(
+				`localUserById: ${localUserByIdCache.hitCount}:${localUserByIdCache.missCount}, ${localUserByIdCache.notFoundCount} n/f`,
+			);
+		if (uriPersonCache.used)
+			logs.push(
+				`uriPerson: ${uriPersonCache.hitCount}:${uriPersonCache.missCount}, ${uriPersonCache.notFoundCount} n/f`,
+			);
+		if (publicKeyCache.used)
+			logs.push(
+				`publicKey: ${publicKeyCache.hitCount}:${publicKeyCache.missCount}, ${publicKeyCache.notFoundCount} n/f`,
+			);
+		if (publicKeyByUserIdCache.used)
+			logs.push(
+				`publicKeyByUserId: ${publicKeyByUserIdCache.hitCount}:${publicKeyByUserIdCache.missCount}, ${publicKeyByUserIdCache.notFoundCount} n/f`,
+			);
 
-	if (logs.length > 0) logger.info(logs.join("\n"));
-}, 1000 * 60);
+		if (logs.length > 0) logger.info(logs.join("\n"));
+	}, 1000 * 60);
 };
